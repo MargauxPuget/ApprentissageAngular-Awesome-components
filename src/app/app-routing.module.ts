7e18152a7360d0fe150a8d1a4a713e987a03868a
MargauxPuget <margaux.puget@gmail.com>
2022-10-20 09:50:10 +0200

    ajout de la route social-media lazy-loaded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'social-media', loadChildren: () => import('./social-media/social-media.module').then(m => m.SocialMediaModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
